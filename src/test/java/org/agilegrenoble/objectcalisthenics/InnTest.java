6a5710baf8e878f56affb38bf5ff659ddb4be500
Johan Martinsson <martinsson.johan@gmail.com>
2013-01-21 21:29:46 +0100

    added assert for sellIn

--- src/test/java/org/agilegrenoble/objectcalisthenics/InnTest.java
+++ src/test/java/org/agilegrenoble/objectcalisthenics/InnTest.java
@@ -12,6 +12,8 @@ public class InnTest {
         inn.updateQuality();
         Iterable<Integer> qualities = extractProperty("quality", Integer.class).from(inn.getItems());
         assertThat(qualities).containsOnly(19, 1, 6, 80, 21, 5);
+        Iterable<Integer> sellIns = extractProperty("sellIn", Integer.class).from(inn.getItems());
+        assertThat(sellIns).containsOnly(9, 1, 4, 0, 14, 2);
     }
 
 }
