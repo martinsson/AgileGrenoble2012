4dfceaa8903a3f1ddfda01485e428e4d350f97cd
Johan Martinsson <martinsson.johan@gmail.com>
2013-02-18 23:07:10 +0100

    made the golden copy of backstage passes and the random creation of backstage passes a bit more explicit renamed names of variables and test methods

--- src/test/java/org/agilegrenoble/objectcalisthenics/GildedRoseTest.java
+++ src/test/java/org/agilegrenoble/objectcalisthenics/GildedRoseTest.java
@@ -11,6 +11,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class GildedRoseTest {
+    private static final int MAX_BACKSTAGE_SELLIN = 30;
+    private static final int MAX_QUALITY = 50;
     private GildedRose gildedRose;
     private List<Item> items;
     private Random rand = new Random(3456789);
@@ -45,7 +47,7 @@ public class GildedRoseTest {
     after_a_shitload_of_days() throws Exception {
          for (int i = 0; i < 500; i++) {
             gildedRose.updateQuality(items);
-        }
+         }
          Iterable<Integer> qualities = extractProperty("quality", Integer.class).from(items);
          assertThat(qualities).containsOnly(0, 50, 0, 80, 0, 0);
          Iterable<Integer> sellIns = extractProperty("sellIn", Integer.class).from(items);
@@ -53,14 +55,14 @@ public class GildedRoseTest {
     }
     
     @Test public void 
-    backstage_pass() throws Exception {
-        List<Item> items2 = aBunchOfBackstagePasses();
-        updateQualityManyTimes(items2);
-        Iterable<Integer> qualities = extractProperty("quality", Integer.class).from(items2);
+    backstage_pass_golden_copy() throws Exception {
+        List<Item> backstagePasses = aBunchOfBackstagePasses();
+        updateQualityManyTimes(backstagePasses);
+        Iterable<Integer> qualities = extractProperty("quality", Integer.class).from(backstagePasses);
         assertThat(qualities).containsOnly(30, 48, 45, 0, 11, 0, 0, 0, 36, 15, 33, 50, 50, 27, 0, 26, 42, 50, 0, 50, 50, 0, 29, 0, 0, 36, 50, 41, 50,
                 0, 49, 25, 0, 12, 0, 50, 0, 0, 0, 43, 0, 50, 23, 27, 33, 0, 0, 37, 0, 43, 0, 0, 45, 50, 22, 43, 0, 30, 14, 44, 50, 0, 17, 0, 17, 50,
                 16, 50, 19, 44, 0, 0, 37, 34, 0, 0, 0, 50, 0, 29, 40, 50, 50, 47, 0, 0, 47, 0, 26, 11, 26, 16, 0, 50, 0, 0, 0, 35, 0, 50);
-        Iterable<Integer> sellIns = extractProperty("sellIn", Integer.class).from(items2);
+        Iterable<Integer> sellIns = extractProperty("sellIn", Integer.class).from(backstagePasses);
         assertThat(sellIns).containsOnly(1, 18, -7, 6, 8, 16, -8, 6, 4, -9, 4, -2, -9, 8, 14, 7, 9, -9, 12, 2, -9, 12, -7, 5, -6, -1, -11, 2, -4, 9,
                 4, 15, 13, -4, -4, 12, -7, 9, -4, -7, 4, 0, 11, 6, -8, 3, 17, 12, 9, -4, 13, -5, 10, 4, 14, 6, 14, 1, -5, -3, 15, 11, -2, -6, -2, 2,
                 -11, 17, 10, 18, 2, 14, -8, -5, 1, -1, 0, 14, 7, 9, -3, 2, -9, -1, -6, 5, -5, 8, -5, 11, 3, 17, 17, 13, 5, 11, 16, -3, 6, 11);
@@ -75,20 +77,22 @@ public class GildedRoseTest {
     private List<Item> aBunchOfBackstagePasses() {
         List<Item> listOfPasses = new ArrayList<Item>();
         for (int i = 0; i < 100; i++) {
-            listOfPasses.add(makeBackstagePass(quality(), sellIn()));
+            listOfPasses.add(aRandomBackstagePass());
         }
         return listOfPasses;
     }
 
-    private int sellIn() {
-        return rand.nextInt(30);
+    private int randomSellIn() {
+        return rand.nextInt(MAX_BACKSTAGE_SELLIN);
     }
 
-    private int quality() {
-        return rand.nextInt(50);
+    private int randomQuality() {
+        return rand.nextInt(MAX_QUALITY);
     }
 
-    private Item makeBackstagePass(int quality, int sellIn) {
+    private Item aRandomBackstagePass() {
+        int quality = randomQuality();
+        int sellIn = randomSellIn();
         return new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
     }
 
